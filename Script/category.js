1b1cd0e04044320053040264707d7b50d1b623ed
Monazir <monazirhussain722@gmail.com>
2022-08-24 23:48:59 +0530

    category

---
+++ Script/category.js
@@ -0,0 +1,7 @@
+var slideList = document.querySelector("#category-list");
+
+var slide = document.querySelector("#category");
+
+slide.addEventListener("onClick", function(){
+    slideList.style.display="block";
+})
