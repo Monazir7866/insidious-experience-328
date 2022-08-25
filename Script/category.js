61ef4934ce395691fe8b79ee3814c1ee401def47
Monazir <monazirhussain722@gmail.com>
2022-08-25 17:46:17 +0530

    slide

--- Script/category.js
+++
@@ -1,7 +0,0 @@
-var slideList = document.querySelector("#category-list");
-
-var slide = document.querySelector("#category");
-
-slide.addEventListener("onClick", function(){
-    slideList.style.display="block";
-})
